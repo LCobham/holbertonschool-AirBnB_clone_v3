385f01d1caa6f41c41007702f1d3cea48c73c561
LCobham <lucascobham@hotmail.com>
2023-04-16 16:25:25 -0700

    Fixed coding style in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,14 +13,17 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix="/api/v1")
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.teardown_appcontext
 def app_teardown(exception):
     """calls close on storage"""
     storage.close()
+
 
 if __name__ == "__main__":
     apiHost, apiPort = getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT")
